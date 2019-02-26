f8e9a25b73080ea70496d9bab3e3a7c61e818a68
DmitriyS87 <schukindmitriy@gmail.com>
2019-02-26 21:09:44 +0800

    moves main.js to entry dir  src/

---
+++ src/main.js
@@ -0,0 +1,103 @@
+const RANDOM_MAX = 50;
+
+const FILTERS = [
+  {
+    id: `filter__all`,
+    text: `ALL`,
+    condition: `checked`
+  },
+  {
+    id: `filter__overdue`,
+    text: `OVERDUE`,
+    condition: `disabled`
+  },
+  {
+    id: `filter__today`,
+    text: `TODAY`,
+    condition: `disabled`
+  },
+  {
+    id: `filter__favorites`,
+    text: `FAVORITES`
+  },
+  {
+    id: `filter__repeating`,
+    text: `Repeating`
+  },
+  {
+    id: `filter__tags`,
+    text: `Tags`
+  },
+  {
+    id: `filter__archive`,
+    text: `ARCHIVE`
+  }
+];
+
+const FILTERS_PATH = document.querySelector(`.main__filter`);
+
+const TASK_2_CARD_COUNT = 7;
+
+const TASKS_BOARD = document.querySelector(`.board__tasks`);
+
+const getRandomCount = () => {
+  return Math.round(Math.random() * RANDOM_MAX);
+};
+
+const createFilterInput = ({id, condition = false}, countTasks, onClickFilter) => {
+  const input = document.createElement(`input`);
+  input.type = `radio`;
+  input.id = id;
+  input.classList.add(`filter__input`, `visually-hidden`);
+  input.name = `filter`;
+  if (condition) {
+    input.setAttribute(condition, condition);
+  }
+  input.addEventListener(`click`, () => {
+    onClickFilter(countTasks);
+  });
+  return input;
+};
+
+const createFilterLabel = ({id, text}, countTasks) => {
+  const label = document.createElement(`label`);
+  label.htmlFor = id;
+  label.classList.add(`filter__label`);
+  label.innerHTML = `${text} <span class="filter__all-count">${countTasks}</span>`;
+  return label;
+};
+
+const pushFilteredCards = (tasksCount) => {
+  const fragmentCards = document.createDocumentFragment();
+  for (let i = 0; i < tasksCount; i++) {
+    fragmentCards.appendChild(taskCard());
+  }
+  TASKS_BOARD.appendChild(fragmentCards);
+};
+
+const clearTasksBoard = () => {
+  TASKS_BOARD.innerHTML = ``;
+};
+
+const taskCard = () => {
+  const templateCard = document.querySelector(`.tmpl__taskcard`);
+  return templateCard.content.cloneNode(true);
+};
+
+const fragment = document.createDocumentFragment();
+
+const fragmentFilters = FILTERS.reduce((result, current) => {
+  const onClickFilter = (countTasks) => {
+    clearTasksBoard();
+    pushFilteredCards(countTasks);
+  };
+
+  const countTasks = getRandomCount();
+  fragment.appendChild(createFilterInput(current, countTasks, onClickFilter));
+  fragment.appendChild(createFilterLabel(current, countTasks));
+  return fragment;
+}, 0);
+
+FILTERS_PATH.appendChild(fragmentFilters);
+
+pushFilteredCards(TASK_2_CARD_COUNT);
