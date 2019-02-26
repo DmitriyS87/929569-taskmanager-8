5254ed3672c95b8fc4e2ba6a9b580c2f8f047daa
DmitriyS87 <schukindmitriy@gmail.com>
2019-02-26 21:34:14 +0800

    makes filter component  module

---
+++ src/filter.js
@@ -0,0 +1,29 @@
+const createFilterInput = ({id, condition = false}, countTasks, onClickFilter) => {
+  const input = document.createElement(`input`);
+  input.type = `radio`;
+  input.id = id;
+  input.classList.add(`filter__input`, `visually-hidden`);
+  input.name = `filter`;
+  if (condition) {
+    input.setAttribute(condition, condition);
+  }
+  input.addEventListener(`click`, () => {
+    onClickFilter(countTasks);
+  });
+  return input;
+};
+
+const createFilterLabel = ({id, text}, countTasks) => {
+  const label = document.createElement(`label`);
+  label.htmlFor = id;
+  label.classList.add(`filter__label`);
+  label.innerHTML = `${text} <span class="filter__all-count">${countTasks}</span>`;
+  return label;
+};
+
+export default (renderData, countTasks, onClickFilter) => {
+  const template = document.createElement(`template`);
+  template.appendChild(createFilterInput(renderData, countTasks, onClickFilter));
+  template.appendChild(createFilterLabel(renderData, countTasks));
+  return template;
+};
