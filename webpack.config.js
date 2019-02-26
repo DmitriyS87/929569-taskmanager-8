acbc419497b4ff086f7bc481099b05f79d10cb0a
DmitriyS87 <schukindmitriy@gmail.com>
2019-02-26 21:08:30 +0800

    fixes syntax bugs webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require(`path`);
 
 module.exports = {
   mode: `development`,
-  entry: `src/main.js`,
+  entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
   devtool: `source-map`,
-  devserver: {
+  devServer: {
     contentBase: path.join(__dirname, `public`),
     publicPath: `http://localhost:8080/`,
     hot: true,
