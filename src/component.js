d7c531263a4e8230ab892f8a24663345d331e30c
DmitriyS87 <schukindmitriy@gmail.com>
2019-03-16 15:41:18 +0800

    adds super Class Component

---
+++ src/component.js
@@ -0,0 +1,41 @@
+import {createElement} from './util';
+
+class Component {
+  constructor() {
+    if (new.target === Component) {
+      throw Error(`Can't instantiate BaseComponent, use only to insert BaseComponent structure to your own components`);
+    }
+
+    this._element = null;
+    this._state = {};
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  get template() {
+    throw new Error(`You have to define template.`);
+  }
+
+  bind() {
+  }
+
+  unbind() {
+  }
+
+  render() {
+    this._element = createElement(this.template);
+    this.bind();
+    return this._element;
+  }
+
+  unrender() {
+    this.unbind();
+    this._element.remove();
+    this._element = null;
+  }
+
+}
+
+export default Component;
